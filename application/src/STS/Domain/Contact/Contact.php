42aad52f4264e6e27f6b4d2f7c0c859318f8f9bc
Jason <jason.fox@naviance.com>
2012-06-20 00:54:50 +0300

    started building out api layer

--- application/src/STS/Domain/Contact/Contact.php
+++ application/src/STS/Domain/Contact/Contact.php
@@ -1,10 +1,25 @@
 <?php
 namespace STS\Domain\Contact\Contact;
 use STS\Domain\Entity\Entity;
+/**
+ * @Entity
+ * @Table(name="contact")
+ */
 class Contact extends Entity
 {
+    /**
+     * @Column
+     */
     protected $name;
+    /**
+     * @Column
+     */
     protected $title;
+    /**
+     * @Column
+     */
     protected $department;
+
+    
     protected $contactDetails;
 }
---
+++ application/tests/integration/STS/Core/Api/DefaultUserFacadeTest.php
@@ -0,0 +1,4 @@
+<?php
+class DefaultUserFacadeTest extends PHPUnit_Framework_TestCase
+{
+}
