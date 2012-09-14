641e85d8dc7c452bd1c0a91dddfb3a677957a002
Jason <jasonrobertfox@gmail.com>
2012-09-14 05:05:32 -0400

    added beginings of school list per 2

---
+++ modules/admin/controllers/SchoolController.php
@@ -0,0 +1,32 @@
+<?php
+use STS\Core;
+
+use STS\Web\Controller\SecureBaseController;
+class Admin_SchoolController extends SecureBaseController
+{
+    protected $schoolFacade;
+
+    public function init()
+    {
+        parent::init();
+        $core = Core::getDefaultInstance();
+        $this->schoolFacade = $core->load('SchoolFacade');
+    }
+
+    public function indexAction()
+    {
+        $this->view->objects = $this->schoolFacade->getSchoolsForSpecification(null);
+        
+        $this->view->layout()->pageHeader = $this->view->partial('partials/page-header.phtml', array(
+                   'title' => 'Schools' ));
+        
+        
+       // $this->view->layout()->pageHeader = $this->view->partial('partials/page-header.phtml', array(
+         //   'title' => 'Schools' , 'add' => 'Add New User' , 
+           // 'addRoute' => '/admin/user/add'
+        //));
+    }
+
+
+}
+
