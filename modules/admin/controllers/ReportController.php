556d3e76e7193ed69da4d7b85d66de630bef3ece
Jason <jasonrobertfox@gmail.com>
2012-11-13 01:38:13 -0500

    updated basic report widgit for presentations

--- modules/admin/controllers/ReportController.php
+++ modules/admin/controllers/ReportController.php
@@ -1,8 +1,17 @@
 <?php
-
+use STS\Core;
 use STS\Web\Controller\SecureBaseController;
 
-class Admin_ReportController extends SecureBaseController {
+class Admin_ReportController extends SecureBaseController
+{
+
+    protected $presentationFacade;
+    public function init()
+    {
+        parent::init();
+        $core = Core::getDefaultInstance();
+        $this->presentationFacade = $core->load('PresentationFacade');
+    }
 
     public function indexAction()
     {
@@ -11,9 +20,35 @@ class Admin_ReportController extends SecureBaseController {
 
     public function presentationAction()
     {
-        $this->view->layout()->pageHeader = $this->view
-            ->partial('partials/page-header.phtml', array(
+        $this->view->layout()->pageHeader = $this->view->partial(
+            'partials/page-header.phtml',
+            array(
                 'title' => 'Presentation Summary Report'
-            ));
+            )
+        );
+        $form = $this->getForm();
+        $params = $this->getRequest()->getParams();
+        //$form->setDefaults($params);
+        if ($form->isValid($params) && array_key_exists('submit', $params)) {
+            $criteria = array('startDate'=>$params['startDate'], 'endDate'=>$params['endDate']);
+            $summary = $this->presentationFacade->getPresentationsSummary($criteria);
+            if($summary->totalPresentations==0){
+                $this->view->noData = true;
+            }
+            $this->view->summary = $summary;
+            $this->view->startDate = $summary->startDate;
+            $this->view->endDate = $summary->endDate;
+        }else{
+            $this->view->noData = true;
+        }
+        $this->view->form = $form;
+        
+        
+    }
+
+    private function getForm()
+    {
+        $form = new \Admin_ReportBasicForm();
+        return $form;
     }
 }
